07f07904cba059c168a933571e453cac171b3f68
Anton Bondarenko <bondarenkoav2581.2@gmail.com>
2025-01-08 11:16:13 +0200

    3 jwt

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require('dotenv').config(); // Додано для завантаження змінних середовища з файлу .env
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const { orders, products } = require('./db');
 
 const app = express();
@@ -17,14 +20,69 @@ app.use(cors({
   origin: 'http://localhost:3000',
 }));
 
+app.use(express.json());
+
+const secretKey = process.env.JWT_SECRET_KEY || 'your-secret-key'; // Використовуйте секретний ключ з конфігурації
+
+// Підключення до MongoDB
+const mongoUri = process.env.MONGO_URI || 'your-mongodb-uri';
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
+});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/api/auth/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username already exists' });
+    }
+
+    const newUser = new User({ username, password });
+    await newUser.save();
+
+    return res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.post('/api/auth/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username, password });
+    if (user) {
+      const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+      return res.status(200).json({ token });
+    } else {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
 app.get('/api/orders', (req, res) => {
   res.json(orders);
 });
 
 app.get('/api/products', (req, res) => {
-    res.json(products);
-  });
-  
+  res.json(products);
+});
 
 let activeSessions = 0;
 
